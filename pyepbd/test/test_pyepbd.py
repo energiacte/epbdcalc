ec37652bbbdb36069bef9666fe6200445666f0cf
Rafael Villar Burke <pachi@ietcc.csic.es>
2016-02-03 11:10:36 +0100

    test: don't duplicate input files in examples

--- pyepbd/test/test_pyepbd.py
+++ pyepbd/test/test_pyepbd.py
@@ -57,77 +57,77 @@ TESTKRDEL = 1.0
 TESTKEXP = 1.0
 
 def test_ejemplo1base():
-    EP = epfromfile('ejemplo1base.csv', TESTKRDEL, TESTKEXP, TESTFP)
+    EP = epfromfile('../examples/ejemplo1base.csv', TESTKRDEL, TESTKEXP, TESTFP)
     assert check(EP, [50.0, 200.0]) == True
 
 def test_ejemplo1base_fail():
-    EP = epfromfile('ejemplo1base.csv', TESTKRDEL, TESTKEXP, TESTFP)
+    EP = epfromfile('../examples/ejemplo1base.csv', TESTKRDEL, TESTKEXP, TESTFP)
     assert check(EP, [53.0, 200.0]) == False
 
 def test_ejemplo1base_normativo():
-    EP = epfromfile('ejemplo1base.csv', TESTKRDEL, TESTKEXP, CTEFP)
+    EP = epfromfile('../examples/ejemplo1base.csv', TESTKRDEL, TESTKEXP, CTEFP)
     assert check(EP, [34.1, 208.20]) == True
 
 def test_ejemplo1PV():
-    EP = epfromfile('ejemplo1PV.csv', TESTKRDEL, TESTKEXP, TESTFP)
+    EP = epfromfile('../examples/ejemplo1PV.csv', TESTKRDEL, TESTKEXP, TESTFP)
     assert check(EP, [75.0, 100.0]) == True
 
 def test_ejemplo1PV_normativo():
-    EP = epfromfile('ejemplo1PV.csv', TESTKRDEL, TESTKEXP, CTEFP)
+    EP = epfromfile('../examples/ejemplo1PV.csv', TESTKRDEL, TESTKEXP, CTEFP)
     assert check(EP, [67.1, 104.1]) == True
 
 def test_ejemplo1xPV():
-    EP = epfromfile('ejemplo1xPV.csv', TESTKRDEL, TESTKEXP, TESTFP)
+    EP = epfromfile('../examples/ejemplo1xPV.csv', TESTKRDEL, TESTKEXP, TESTFP)
     assert check(EP, [120.0, -80.0]) == True
 
 def test_ejemplo1xPV_normativo():
-    EP = epfromfile('ejemplo1xPV.csv', TESTKRDEL, TESTKEXP, CTEFP)
+    EP = epfromfile('../examples/ejemplo1xPV.csv', TESTKRDEL, TESTKEXP, CTEFP)
     assert check(EP, [120.0, -80.0]) == True
 
 def test_ejemplo1xPVk0():
-    EP = epfromfile('ejemplo1xPV.csv', TESTKRDEL, 0.0, TESTFP)
+    EP = epfromfile('../examples/ejemplo1xPV.csv', TESTKRDEL, 0.0, TESTFP)
     assert check(EP, [100.0, 0.0]) == True
 
 def test_ejemplo1xPVk0_normativo():
-    EP = epfromfile('ejemplo1xPV.csv', TESTKRDEL, 0.0, CTEFP)
+    EP = epfromfile('../examples/ejemplo1xPV.csv', TESTKRDEL, 0.0, CTEFP)
     assert check(EP, [100.0, 0.0]) == True
 
 def test_ejemplo2xPVgas():
-    EP = epfromfile('ejemplo2xPVgas.csv', TESTKRDEL, TESTKEXP, TESTFP)
+    EP = epfromfile('../examples/ejemplo2xPVgas.csv', TESTKRDEL, TESTKEXP, TESTFP)
     assert check(EP, [30.0, 169.0]) == True
 
 def test_ejemplo2xPVgas_normativo():
-    EP = epfromfile('ejemplo2xPVgas.csv', TESTKRDEL, TESTKEXP, CTEFP)
+    EP = epfromfile('../examples/ejemplo2xPVgas.csv', TESTKRDEL, TESTKEXP, CTEFP)
     assert check(EP, [30.9, 186.1]) == True
 
 def test_ejemplo3PVBdC():
-    EP = epfromfile('ejemplo3PVBdC.csv', TESTKRDEL, TESTKEXP, TESTFP)
+    EP = epfromfile('../examples/ejemplo3PVBdC.csv', TESTKRDEL, TESTKEXP, TESTFP)
     assert check(EP, [180.0, 38.0]) == True
 
 def test_ejemplo3PVBdC_normativo():
-    EP = epfromfile('ejemplo3PVBdC.csv', TESTKRDEL, TESTKEXP, CTEFP)
+    EP = epfromfile('../examples/ejemplo3PVBdC.csv', TESTKRDEL, TESTKEXP, CTEFP)
     assert check(EP, [177.5, 39.6]) == True
 
 def test_ejemplo4cgnfosil():
-    EP = epfromfile('ejemplo4cgnfosil.csv', TESTKRDEL, TESTKEXP, TESTFP)
+    EP = epfromfile('../examples/ejemplo4cgnfosil.csv', TESTKRDEL, TESTKEXP, TESTFP)
     assert check(EP, [-14.0, 227.0]) == True
 
 def test_ejemplo4cgnfosil_normativo():
-    EP = epfromfile('ejemplo4cgnfosil.csv', TESTKRDEL, TESTKEXP, CTEFP)
+    EP = epfromfile('../examples/ejemplo4cgnfosil.csv', TESTKRDEL, TESTKEXP, CTEFP)
     assert check(EP, [-12.7, 251]) == True
 
 def test_ejemplo5cgnbiogas():
-    EP = epfromfile('ejemplo5cgnbiogas.csv', TESTKRDEL, TESTKEXP, TESTFP)
+    EP = epfromfile('../examples/ejemplo5cgnbiogas.csv', TESTKRDEL, TESTKEXP, TESTFP)
     assert check(EP, [159.0, 69.0]) == True
 
 def test_ejemplo5cgnbiogas_normativo():
-    EP = epfromfile('ejemplo5cgnbiogas.csv', TESTKRDEL, TESTKEXP, CTEFP)
+    EP = epfromfile('../examples/ejemplo5cgnbiogas.csv', TESTKRDEL, TESTKEXP, CTEFP)
     assert check(EP, [148.9, 76.4]) == True
 
 def test_ejemplo6K3():
-    EP = epfromfile('ejemplo6K3.csv', TESTKRDEL, TESTKEXP, TESTFP)
+    EP = epfromfile('../examples/ejemplo6K3.csv', TESTKRDEL, TESTKEXP, TESTFP)
     assert check(EP, [1385.5, -662]) == True
 
 def test_ejemplo6K3_normativo():
-    EP = epfromfile('ejemplo6K3.csv', TESTKRDEL, TESTKEXP, CTEFP)
+    EP = epfromfile('../examples/ejemplo6K3.csv', TESTKRDEL, TESTKEXP, CTEFP)
     assert check(EP, [1385.5, -662]) == True
